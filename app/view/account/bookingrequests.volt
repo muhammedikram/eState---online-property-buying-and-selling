eb48cc0f40598d7bc7501a68f895d19171b00d64
Muhammed Ikram <ikimuhammed@hotmail.com>
2016-02-26 07:33:29 +0000

    made few tweeks to booking requests

--- app/view/account/bookingrequests.volt
+++ app/view/account/bookingrequests.volt
@@ -2,7 +2,7 @@
 	<h2>Viewings Requests</h2>
 <br><br>
 	<div class="col-md-12">
-		<table class="table table-hover">
+		<table class="table">
 			{% if viewings is defined %}
 			{% if viewings|length > 0 %}
 			<tr>
@@ -10,15 +10,23 @@
 				<th>Viewer Name</th>
 				<th>Date</th>
 				<th>Time</th>
+				<th></th>
 			</tr>
 			
 				{% for row in viewings %}
 
-				<tr>
+				<?php
+					$enabled = $row->getEnabled();
+
+					if ($enabled == 0)
+					{
+					?>
+				<tr div class="jumbotron">
 					<td>{{row.getViewingID()}}</td>
 					<td>{{row.getName()}}</td>
 					<td>{{row.getDate()}}</td>
 					<td>{{row.getTime()}}</td>
+
 					<td>
 						<form action="/account/bookingrequests/{{row.getpropertyID()}}" method="post">
 							 <button type="submit" class="btn btn-sm btn-success" name="action" value="approve" value="approve">
@@ -30,6 +38,33 @@
 
 					</td>
 				</tr>
+				<?php  }  ?>
+
+				<?php
+					if ($enabled == 1)
+					{
+				?>
+				<tr>
+					<td>{{row.getViewingID()}}</td>
+					<td>{{row.getName()}}</td>
+					<td>{{row.getDate()}}</td>
+					<td>{{row.getTime()}}</td>
+					<td>
+						<form action="/account/bookingrequests/{{row.getpropertyID()}}" method="post">
+							 <button type="submit" disabled class="btn btn-sm btn-success" name="action" value="approve" value="approve">
+                				<span class="glyphicon glyphicon-ok"></span>
+                   				Already Approved
+           					 </button>
+             
+						</form>
+
+					</td>
+				</tr>
+
+				
+
+
+				<?php  }  ?>
 					{% endfor %}
 							{% else %}
 				<div class='jumbotron text-center'>No any property viewing requests are been received.</div>
--- app/view/account/bookingrequests.volt.php
+++ app/view/account/bookingrequests.volt.php
@@ -2,7 +2,7 @@
 	<h2>Viewings Requests</h2>
 <br><br>
 	<div class="col-md-12">
-		<table class="table table-hover">
+		<table class="table">
 			<?php if (isset($viewings)) { ?>
 			<?php if ($this->length($viewings) > 0) { ?>
 			<tr>
@@ -10,15 +10,23 @@
 				<th>Viewer Name</th>
 				<th>Date</th>
 				<th>Time</th>
+				<th></th>
 			</tr>
 			
 				<?php foreach ($viewings as $row) { ?>
 
-				<tr>
+				<?php
+					$enabled = $row->getEnabled();
+
+					if ($enabled == 0)
+					{
+					?>
+				<tr div class="jumbotron">
 					<td><?php echo $row->getViewingID(); ?></td>
 					<td><?php echo $row->getName(); ?></td>
 					<td><?php echo $row->getDate(); ?></td>
 					<td><?php echo $row->getTime(); ?></td>
+
 					<td>
 						<form action="/account/bookingrequests/<?php echo $row->getpropertyID(); ?>" method="post">
 							 <button type="submit" class="btn btn-sm btn-success" name="action" value="approve" value="approve">
@@ -30,6 +38,33 @@
 
 					</td>
 				</tr>
+				<?php  }  ?>
+
+				<?php
+					if ($enabled == 1)
+					{
+				?>
+				<tr>
+					<td><?php echo $row->getViewingID(); ?></td>
+					<td><?php echo $row->getName(); ?></td>
+					<td><?php echo $row->getDate(); ?></td>
+					<td><?php echo $row->getTime(); ?></td>
+					<td>
+						<form action="/account/bookingrequests/<?php echo $row->getpropertyID(); ?>" method="post">
+							 <button type="submit" disabled class="btn btn-sm btn-success" name="action" value="approve" value="approve">
+                				<span class="glyphicon glyphicon-ok"></span>
+                   				Already Approved
+           					 </button>
+             
+						</form>
+
+					</td>
+				</tr>
+
+				
+
+
+				<?php  }  ?>
 					<?php } ?>
 							<?php } else { ?>
 				<div class='jumbotron text-center'>No any property viewing requests are been received.</div>
